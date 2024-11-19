884432bb781486e918422aedf45dfa62b1a9257b
Boogle <theboogledoo@gmail.com>
2024-11-19 14:03:06 -0600

    oops, forgot to replace my name :pensive:

--- SkeletonKey/Plugin.cs
+++ SkeletonKey/Plugin.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace YourNamespace
 {
-    [BepInPlugin("com.yourname.removekeyid", "Remove KeyID Requirement", "1.0.0")]
+    [BepInPlugin("com.boogle.removekeyid", "Adds the Skeleton Key Item", "1.0.1")]
     public class RemoveKeyIDPlugin : BaseUnityPlugin
     {
         public void Awake()
